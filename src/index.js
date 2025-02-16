b8da885317e3b4ec3f4875b1d784d08f0a8eefe3
gokul-krishna-raj <gokulraj201@gmail.com>
2025-02-16 21:24:15 +0530

    updated the changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
-import { Provider  } from 'react-redux';
-import { store } from './store/store';
-
+import Routes from './routes/Routes';
+import store from './store';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  // <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
